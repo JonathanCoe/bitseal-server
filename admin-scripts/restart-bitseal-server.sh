687117647e9553a706b81824bc562a09cf3ce333
JonathanCoe <jcoe@protonmail.ch>
2014-11-24 19:35:58 +0100

    Minor update to make times used in admin scripts more consistent

--- admin-scripts/restart-bitseal-server.sh
+++ admin-scripts/restart-bitseal-server.sh
@@ -12,8 +12,8 @@ fi
 # Before we try to restart PyBitmessage, check whether it is still running (perhaps doing cleanup)
 ps cax | grep python > /dev/null
 if [ $? -eq 0 ]; then
-  # If PyBitmessage  is still running, give it another 60 seconds
-  sleep 60 
+  # If PyBitmessage  is still running, give it another 30 seconds
+  sleep 30 
 else
   nohup python bitmessagemain.py >> ~/apps/bitseal-server/src/nohup.out 2>&1 &
   exit
@@ -22,8 +22,8 @@ fi
 # Check again whether it is safe to restart PyBitmessage
 ps cax | grep python > /dev/null
 if [ $? -eq 0 ]; then
-  # If PyBitmessage  is still running, give it another 60 seconds
-  sleep 60
+  # If PyBitmessage  is still running, give it another 30 seconds
+  sleep 30
 else
   nohup python bitmessagemain.py >> ~/apps/bitseal-server/src/nohup.out 2>&1 &
   exit
