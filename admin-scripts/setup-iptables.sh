f3d2806dc9109a1b37a9554335f987261dfd6ca6
Jonathan Coe <jcoe@protonmail.ch>
2015-02-17 10:37:30 +0000

    Updated the port numbers in setup-ip-tables.sh

--- admin-scripts/setup-iptables.sh
+++ admin-scripts/setup-iptables.sh
@@ -5,8 +5,8 @@ iptables -A INPUT -p tcp --tcp-flags ALL NONE -j DROP
 iptables -A INPUT -p tcp ! --syn -m state --state NEW -j DROP
 iptables -A INPUT -p tcp --tcp-flags ALL ALL -j DROP
 iptables -A INPUT -i lo -j ACCEPT
-iptables -A INPUT -p tcp -m tcp --dport 8444 -j ACCEPT
-iptables -A INPUT -p tcp -m tcp --dport 8442 -j ACCEPT
+iptables -A INPUT -p tcp -m tcp --dport 8446 -j ACCEPT
+iptables -A INPUT -p tcp -m tcp --dport 8447 -j ACCEPT
 iptables -A INPUT -p tcp -m tcp --dport 22 -j ACCEPT
 iptables -I INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
 iptables -P OUTPUT ACCEPT
