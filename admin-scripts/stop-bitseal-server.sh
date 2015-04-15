e7ab0dad956c6c91b11c86f69931243410ef2ae0
Jonathan Coe <jcoe@protonmail.ch>
2015-04-15 22:45:17 +0100

    Added admin script for stopping bitseal-server

---
+++ admin-scripts/stop-bitseal-server.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#Stop bitseal-server, giving it a reasonable amount of time to shut down gracefully
+ps aux | grep -i bitseal-server.py | awk {'print $2'} | xargs kill -2
+sleep 30
+
+# If bitseal-server has still not closed properly, force kill it
+ps cax | grep python > /dev/null
+if [ $? -eq 0 ]; then
+  ps aux | grep -i bitseal-server.py | awk {'print $2'} | xargs kill -9
+  sleep 10
+fi
